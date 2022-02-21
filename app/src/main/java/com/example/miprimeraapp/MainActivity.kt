66e060c485ee23d5ffac8b189c14de765e255f9c
EmilyBalam <72897197+EmilyBalam@users.noreply.github.com>
2022-02-20 20:12:18 -0600

    LinearLayout Utilizando recursos

--- app/src/main/java/com/example/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.miprimeraapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    /*fun validarDato():Boolean{
+        val etNombre=findViewById<EditText>(R.id.editTextTextPersonName)
+        val nombreUsuario = etNombre.text
+
+        if(nombreUsuario.isNullOrEmpty()){
+            return false
+        }
+return true */
+
 }
